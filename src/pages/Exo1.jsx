ff9561e80fb6d8abec465433c0973ce1531b0bcd
VanessDev <vanessa.biamonti@gmail.com>
2025-12-11 16:51:32 +0100

    commit5

--- src/pages/Exo1.jsx
+++ src/pages/Exo1.jsx
@@ -3,11 +3,16 @@ import Title from "../components/Title";
 import Message from "../components/Message";
 import Calculator from "./Calculator";
 import Buttons from "./Buttons";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 
 function Exo1() {
+  
   return (
     <>
+
+    
       <section className="exercice">
         <div>
           <Title />
